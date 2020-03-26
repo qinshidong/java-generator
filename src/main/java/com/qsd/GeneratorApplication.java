22c90f570f1105fe9499b450e7afd31c295a1dc9
qinshidong <1477903539@qq.com>
2020-03-26 17:37:44 +0800

    shouci

---
+++ src/main/java/com/qsd/GeneratorApplication.java
@@ -0,0 +1,18 @@
+
+package com.qsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * renren-generator
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+@SpringBootApplication
+public class GeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorApplication.class, args);
+	}
+}
